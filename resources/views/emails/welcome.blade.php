c2b7a3cb5d473af59d5763b0ac213833b0f5c0a2
Brian Aydemir <baydemir@continuousassurance.org>
2019-03-06 10:24:19 -0600

    1.34.1-359.sib_Production_Build

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -13,38 +13,18 @@
 <div>
 	<img id="logo"
 src="{{ $logo }}" alt="logo" />
-	<p>Greetings {{ $user->getFullName() }},</p>
-	On behalf of the staff at the Software Assurance Marketplace: thank you for your interest in our services!
-	<p>Please take a moment to view the <a
+
+	<h3>Welcome</h3>
+	Greetings {{ $user->getFullName() }}. On behalf of the staff at the Software Assurance Marketplace: thank you for your interest in our services! Please take a moment to view the <a
 href="https://www.swampinabox.org/doc/SWAMP-User-Manual.pdf">SWAMP User Manual</a> and the <a
-href='https://continuousassurance.org/video-tutorials/'>video tutorials</a> on our website; they provide valuable guidance for the initial orientation experience. </p>
-	<p>
-		<div style="float: left; width: 30%;">
-			<p style="font-size:14px">&nbsp;</p>
-			<p style="font-size:14px">Need Help?</p>
-			<p style="font-size:14px">&nbsp;</p>
-		</div>
-		<div style="float: right; width: 70%;">
-			<p style="font-size:14px">Failed Analysis?</p>
-			<p style="font-size:14px">Broken Package?</p>
-			<p style="font-size:14px">Confusing scheduler?</p>
-		</div>
-	</p>
-	<p style="font-size:14px">We’re here to help with that!</p>
-	We will gladly assist you for a productive and worthwhile experience, including analyzing failed assessment results for you, assisting with package content and package upload questions, and generally assuring that you are confident in your ability to use this service productively.
-	<p>
-		<p style="color:red;font-size:18px">Technical Support</p>
-		The SWAMP offers 24/7 support 365 days a year.  Following is contact information for the SWAMP:
-		<ul>
-			<li>Dial: (317) 274-3942 (24/7/365)</li>
-			<li>Email: <a href="mailto: support@continuousassurance.org">support@continuousassurance.org</a></li>
-		</ul>
-		To create a support ticket, go to
-		<a href="https://ticket.continuousassurance.org">https://ticket.continuousassurance.org</a>
-		or email
-		<a href="mailto: support@continuousassurance.org">support@continuousassurance.org</a>
-	</p>
-	We look forward to ensuring your success here.
+href='https://continuousassurance.org/video-tutorials/'>video tutorials</a> on our website; they provide valuable guidance for the initial orientation experience.
+
+	<h3>Need help?</h3>
+	Failed analysis? Problems creating a package? We’re here to help! We will gladly assist you for a productive and worthwhile experience, including analyzing failed assessment results for you, assisting with package content and package upload questions, and generally assuring that you are confident in your ability to use this service productively.
+
+	<h3>Technical Support</h3>
+	For support, contact the SWAMP at: <a href="mailto: support@continuousassurance.org">support@continuousassurance.org</a>. We look forward to ensuring your success.
+
 	<p>-The Software Assurance Marketplace (SWAMP)</p>
 </div>
 </body>
